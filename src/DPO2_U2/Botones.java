9433966646a877ed80238cbe4595c0f74362b700
Andres Ramos <86693032+AndyRmCz@users.noreply.github.com>
2022-03-29 14:52:07 -0600

    Buttons style test

---
+++ src/DPO2_U2/Botones.java
@@ -0,0 +1,13 @@
+package DPO2_U2;
+import javax.swing.JButton;
+
+public class Botones extends JButton;
+
+
+
+botones(String txt){
+    this.setText(txt);
+    this.setBackground(null);
+    this.setForeground(Color.WHITE);
+    //this.setBounds(250 w,30 h);
+}
